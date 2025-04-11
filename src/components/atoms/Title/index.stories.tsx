26d1ac8de585c6b4e0c7beb12c9966b31f3fa49c
jchyeon0814 <jchyeon0814@gmail.com>
2025-04-11 22:21:01 +0900

    임시저장

---
+++ src/components/atoms/Title/index.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Title } from '.';
+
+const meta = {
+    title: 'Atoms/Title',
+    component:Title,
+} satisfies Meta<typeof Title>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    
+};
