e1134060271170bb27dbb405b4765a55634b35fa
jchyeon0814 <jchyeon0814@gmail.com>
2025-04-15 21:42:11 +0900

    Button 스토리 추가

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { Button } from '.';
+
+const meta = {
+    title: 'atoms/Button',
+    component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        label: "-",
+        onClick: fn(),
+    }
+}
